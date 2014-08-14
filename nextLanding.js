d61398a7b1ffec60ac58830ac2ca320cbb0da86c
Jonathan Mariano <jonmariano21@gmail.com>
2014-08-14 13:46:21 -0400

    makes new rows in table

--- nextLanding.js
+++ nextLanding.js
@@ -1,8 +1,12 @@
+/*
 var queue=[];
 
-var click_button = $("#clickMe");
+var addMe_button = $("#addMe");
 
-click_button.click(function(){
+addMe_button.click(function(){
+
+	var date = new Date();//Creates obj w/current date and time
+	
 	var getval = ($("#nameField").val());
 	var getval2 = ($("#labNum").val());
 	
@@ -17,4 +21,55 @@ click_button.click(function(){
 	queue.push(getval2);
 
 	alert(queue);
+	alert(date);
+	
 });
+
+*/
+
+function addRow(){
+
+	var time = new Date();//Creates obj w/current date and time
+	
+	var name = document.getElementById("nameField");
+	var labNum = document.getElementById("labNum");
+	var terminalNum = document.getElementById("terminalNum");
+	var table = document.getElementById("cse12Table");
+	
+	var rowCount = table.rows.length;//number of rows
+	
+	var row = table.insertRow(rowCount);//insert new row into index of rowCount
+
+	row.insertCell(0).innerHTML= rowCount;
+	row.insertCell(1).innerHTML = name.value;
+	row.insertCell(2).innerHTML = labNum.value;
+	row.insertCell(3).innerHTML = terminalNum.value;
+	row.insertCell(4).innerHTML = time;
+			
+}
+
+
+
+
+function load(){
+	console.log("page load finished bitch!");
+}
+
+
+
+/*
+function addRow() {
+         
+    var myName = document.getElementById("name");
+    var age = document.getElementById("age");
+    var table = document.getElementById("myTableData");
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+    row.insertCell(1).innerHTML= myName.value;
+    row.insertCell(2).innerHTML= age.value;
+
+}
+*/

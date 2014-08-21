915b66c69ac86897f6773aab80aa6fea77ddcc45
Jonathan Mariano <jonmariano21@gmail.com>
2014-08-21 17:12:48 -0400

    BUG: deletes entire students directory in FB
    When delete button is clicked in table, it deletes the entire students
    directory in FB rather than individual student

--- nextLanding.js
+++ nextLanding.js
@@ -3,19 +3,38 @@ var TABLE_SIZE = 3;//limit number of students displayed in table
 
 //FireBase reference
 var myFB = new Firebase("https://brilliant-fire-8581.firebaseIO.com");
-var studentListRef = myFB.child("students");
+var studentListFB = myFB.child("students");
+//var studentRef = studentListFB.child("BOOO");
 
 //mapping of firebase locations to HTML elements, so we can move / remove elements as necessary.
 var students = {};
+/*
+var table = document.getElementById("cse12Table");
+var rowCount = table.rows.length;//number of rows
+*/
+
+var rowCounter;
+
+//Count all children
+myFB.on("value", function(snapshot){
+	rowCounter = 0;
+	snapshot.forEach(function(){
+		rowCounter += 1;
+	});
+});
+
+
+
+
 
 //FUNCTION: 
 //	takes a new student snapshot and adds an appropriate row to table.
 //===================================================================================================
 function addNewStudent(studentSnapshot) {
 
-	var newRow = $("<tr/>");
+	var newRow = $("<tr id='id'/>");	
 	
-	newRow.append($("<td/>").text("fix this"));    
+	newRow.append($("<td/>").text("fix"));    
 	newRow.append($("<td/>").text(studentSnapshot.val().NAME));
 	newRow.append($("<td/>").text(studentSnapshot.val().LAB_NUMBER));
 	newRow.append($("<td/>").text(studentSnapshot.val().TERMINAL_NUMBER));
@@ -23,56 +42,112 @@ function addNewStudent(studentSnapshot) {
 	//append takes in a htmlstring so have to convert time as a number to a string using toString()
 	newRow.append($("<td/>").text(studentSnapshot.val().TIME));
 	
-	newRow.append($("<td/>").innerHTML = '<button class="btn btn-danger" onclick="removeButton()">Remove</button>');
+	newRow.append($("<td/>").innerHTML = '<button class="removeButton"> x </button>');
+
 	
+/*
+	var one = "<td>fix</td>";    
+	var two = "<td>"+studentSnapshot.val().NAME+"</td>";
+	var three = "<td>" + studentSnapshot.val().LAB_NUMBER + "</td>";
+	var four = "<td>" + studentSnapshot.val().TERMINAL_NUMBER + "</td>";
+	
+	//append takes in a htmlstring so have to convert time as a number to a string using toString()
+	var five = "<td>" + studentSnapshot.val().TIME + "</td>";
+	
+	var six = "<td class='removeButton'> x </td>";
+	
+*/	
 	//Store student reference to the table row.
 	students[studentSnapshot.name()] = newRow;
 	
-	$("#cse12Table").append(newRow);
+	$("#cse12Table").append(newRow);		
+	//$("#cse12Table").append("<tr id=id"+ snapshot.name() + ">" + one+two+three+four+five+six+ "</tr>");
 	
 }//Close: addNewStudent(studentSnapshot)
 
 
 // Create a view to only receive callbacks for the first students
-var studentListView = studentListRef.startAt().limit(TABLE_SIZE);
+var studentListView = studentListFB.startAt().limit(TABLE_SIZE);
 
 //callback to handle when a new student is added.
 studentListView.on('child_added', function (newstudentSnapshot) {
 	addNewStudent(newstudentSnapshot);
+
 });
 
 
 //Press button to add student to list.
-$("#fuckMe").click(function(){
- 
-	 //getDate() returns the day of the month from (1-31)
-	 //getTime() returns the number of milliseconds since midnight Jan 1, 1970
-	 var timeStamp = new Date().getTime();
-	 //alert("time in ms: "+timeStamp);
-	 
-	 //CONVERT the timeStamp number to a string
-	 var timeString = timeStamp.toString();
-	 //alert("time converted to string: "+timeString);
+$("#addMe").click(function(){
+ 	 
+	//getDate() returns the day of the month from (1-31)
+	//getTime() returns the number of milliseconds since midnight Jan 1, 1970
+	var timeStamp = new Date().getTime();
 	
-	  var labNum = Number( $("#labNumInput").val() );//has to be a NUMBER
-	  var name = $("#nameInput").val();
-	  var terminalNum = Number( $("#terminalNumInput").val() );//has to be a NUMBER
+	//CONVERT the timeStamp number to a string
+	var timeString = timeStamp.toString();
 	
-	  if (name.length === 0){
-	  	alert("Please enter your NAME.")
-	    return;
-	  }
+	var labNum = Number( $("#labNum").val() );//has to be a NUMBER
+	var name = $("#nameField").val();
+	var terminalNum = Number( $("#terminalNum").val() );//has to be a NUMBER
 	
-	  var studentRef = studentListRef.child(name);
+	if (name.length === 0){
+		alert("Please enter your NAME.")
+		return;
+	}
 	
-	  // Use setWithPriority to put the name / lab# / terminal# in Firebase, and set the priority to be the time.
-	  studentRef.setWithPriority({ NAME:name, LAB_NUMBER:labNum, TERMINAL_NUMBER:terminalNum, TIME:timeString },  timeString);
+	var studentRef = studentListFB.child(name);
+	
+	// Use setWithPriority to put the name / lab# / terminal# in Firebase, and set the priority to be the time.
+	studentRef.setWithPriority({ NAME:name, LAB_NUMBER:labNum, TERMINAL_NUMBER:terminalNum, TIME:timeString },  timeString);
   
 });
 
+
+
+//FUNCTION: Delete Row
+//	delete row from table.
+//===================================================================================================
+
+myFB.limit(20).on("child_removed", function (snapshot) {
+    alert("inside child_removed");
+
+    console.log('deleting ' + snapshot.name())
+    $('#id' + snapshot.name()).remove()
+     alert("END child_removed");
+});
+
+
+$(document).on('click', '.removeButton', function () {
+    
+    alert("lets delete");
+    
+    var id = studentListFB.name(); //Finds id = students to remove in FireBase
+    
+    alert("id = "+id);
+     
+    myFB.child(id).once('value', function (snapshot) {
+    
+         alert("after child(id)");
+         
+        snapshot.ref().remove();
+        
+         alert("FINALLY REMOVE");
+		 
+
+    });
+
+});
+
+
+
+
+
+
+/*
 //REMOVE
 function removeButton(){
-	document.getElementById("cse12Table").deleteRow();
+	document.getElementById("cse12Table").deleteRow(1);
+
 }
 
 
@@ -103,35 +178,8 @@ function removeButton(){
 
 
 
-/*
-var queue=[];
-
-var addMe_button = $("#addMe");
-
-addMe_button.click(function(){
-
-	var date = new Date();//Creates obj w/current date and time
-	
-	var getval = ($("#nameField").val());
-	var getval2 = ($("#labNum").val());
-	
-	
-	var tst = $("#test");
-	var tst2 = $("#test2");
-	
-	tst.text(getval);
-	tst2.text(getval2);
-	
-	queue.push(getval);
-	queue.push(getval2);
-
-	alert(queue);
-	alert(date);
-	
-});
 
 */
-
 
 
 
@@ -176,9 +224,6 @@ function addRow(){
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
- 
-
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -190,9 +235,9 @@ function addRow(){
 
 
 
-
-/* Delete Row in Table
-================================================== 	*/
+/*
+// Delete Row in Table
+================================================== 	//
 
 function deleteRow(object){
 
@@ -211,7 +256,7 @@ alert("rowCount = "+rowCount);
 
 
 
-
+*/
 function load(){
 	console.log("page load finished bitch!");
 }
